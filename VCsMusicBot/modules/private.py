62e73dc0458ecb476de018b95d7110576f88c1f3
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-11-26 19:02:14 +0530

    Update private.py

--- VCsMusicBot/modules/private.py
+++ VCsMusicBot/modules/private.py
@@ -5,55 +5,9 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from VCsMusicBot.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
 logging.basicConfig(level=logging.INFO)
 
-@Client.on_message(filters.private & filters.incoming & filters.command(['start']))
-def _start(client, message):
-    client.send_message(message.chat.id,
-        text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
-        parse_mode="markdown",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "➕ Add me to your Group ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-                [
-                    InlineKeyboardButton(
-                        "👥 Group", url=f"https://t.me/{SUPPORT_GROUP}"), 
-                    InlineKeyboardButton(
-                        "Channel 📢", url=f"https://t.me/{UPDATES_CHANNEL}")
-                ],[
-                    InlineKeyboardButton(
-                        "🔥 Source Code 🔥", url=f"https://{SOURCE_CODE}")
-                ]
-            ]
-        ),
-        reply_to_message_id=message.message_id
-        )
-
-@Client.on_message(filters.command(["start","start@VCsMusicBot"]) & ~filters.private & ~filters.channel)
-async def gstart(_, message: Message):
-    await message.reply_text(
-        f"""**{PROJECT_NAME} is online.**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "💬 Support Chat", url=f"https://t.me/{SUPPORT_GROUP}"
-                    )
-                ],    
-                [    
-                    InlineKeyboardButton(
-                        "🔎 Search YT", switch_inline_query_current_chat=""
-                    ),
-                    InlineKeyboardButton(
-                        "Close ❌", callback_data="close"
-                    )
-                ]
-            ]
-        ),
-    )
 
 
-@Client.on_message(filters.private & filters.incoming & filters.command(['help']))
+@Client.on_message(filters.private & filters.incoming & filters.command(['helpmusic']))
 def _help(client, message):
     client.send_message(chat_id = message.chat.id,
         text = tr.HELP_MSG[1],
@@ -64,7 +18,7 @@ def _help(client, message):
         reply_to_message_id = message.message_id
     )
 
-help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
+help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('helpmusic+'))
 
 @Client.on_callback_query(help_callback_filter)
 def help_answer(client, callback_query):
